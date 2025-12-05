085f546c5c2ad663223703ecc5fbed63d6209070
quocmoet195 <quocvan140519@gmail.com>
2025-12-05 16:03:46 +0400

    Fix project AppHost

--- Hospital.AppHost/Program.cs
+++ Hospital.AppHost/Program.cs
@@ -1,12 +1,31 @@
-var builder = DistributedApplication.CreateBuilder(args);
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+// Cấu hình MySQL
 var password = builder.AddParameter("DatabasePassword");
 var mysql = builder.AddMySql("mysql")
-                .WithEnvironment("Password", password);
+                 .WithImage("mysql", "9.4")
+                 .WithEnvironment("Password", password);
 
-var hospitalDb = mysql.AddDatabase("HospitalDb"); 
+var hospitalDb = mysql.AddDatabase("HospitalDb");
 
+// Cấu hình NATS
+var natsUser = builder.AddParameter("NatsLogin");
+var natsPass = builder.AddParameter("NatsPassword");
+//var nats = builder
+//    .AddNats("hospital-nats", userName: natsUser, password: natsPass)
+//    .WithJetStream();
+var nats = builder.AddNats("hospital-nats").WithImage("nats", "2.11");
+
+// 1. THÊM GENERATOR SERVICE
+// Generator Service tạo contracts và gửi qua NATS, nên nó cần tham chiếu NATS.
+var generator = builder.AddProject<Projects.Hospital_Generator_Nats_Host>("generator")
+                     .WithReference(nats);
+
+// 2. Cấu hình API Host (Server)
 var api = builder.AddProject<Projects.Hospital_Api_Host>("api")
             .WithReference(hospitalDb)
-            .WaitFor(hospitalDb);
+            .WithReference(nats)
+            .WaitFor(hospitalDb)
+            .WaitFor(nats);
 
 builder.Build().Run();
